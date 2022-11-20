45bef2d3c4c9a8b129d282298e1b4d2f23ab232c
latristain <116692685+latristain@users.noreply.github.com>
2022-11-20 11:34:32 -0600

    Add files via upload

---
+++ Performance.py
@@ -0,0 +1,24 @@
+#In order to run adequately the Performance in the Imperative.py code you have to change the graph that is to be compared
+
+#Import Modules
+import timeit
+
+#function returns the number of seconds it took to execute the code for the Recursive Code
+Recursive_Time = (timeit.timeit(stmt='floyd', setup='from RecursiveCode import floyd', number=1))
+
+#function returns the number of seconds it took to execute the code for the Imperative Code
+Imperative_Time=(timeit.timeit(stmt='floyd', setup='from Imperative import floyd', number=1))
+
+
+#Difference in time between Recursive vs Imperative Time
+dif_Time = Recursive_Time - Imperative_Time
+
+#Print the Results
+print("The execution time taken by the function floyd in the recursive way takes:")
+print(Recursive_Time)
+
+print("The execution time taken by the function floyd in the Imperative way takes:")
+print(Imperative_Time)
+
+print("The difference in time between the execution time taken by the function floyd in the Recursive - Imperative way takes:")
+print (dif_Time)
